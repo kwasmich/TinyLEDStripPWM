7cf3f27cd3ac508ddb5613862e0abb51a2febf9c
Michael Kwasnicki <github@kwasi-ich.de>
2015-08-23 19:14:48 +0200

    * enforcing astyle coding style to all files

--- main.c
+++ main.c
@@ -64,115 +64,139 @@ volatile uint8_t b = 0;
 
 
 static void pwm() {
-//    if ( cnt == 0 ) {
-//        if ( r > cnt ) BIT_SET( PORTB, _BV( R_PIN ) );
-//        if ( g > cnt ) BIT_SET( PORTB, _BV( G_PIN ) );
-//        if ( b > cnt ) BIT_SET( PORTB, _BV( B_PIN ) );
-//    }
-//    
-//    if ( cnt >= r ) {
-//        BIT_CLR( PORTB, _BV( R_PIN ) );
-//    }
-//    
-//    if ( cnt >= g ) {
-//        BIT_CLR( PORTB, _BV( G_PIN ) );
-//    }
-//    
-//    if ( cnt >= b ) {
-//        BIT_CLR( PORTB, _BV( B_PIN ) );
-//    }
-//    
-//    cnt++;
-    
-    if ( cnt < 4 ) {
-        if ( r > cnt ) BIT_SET( PORTB, _BV( R_PIN ) );
-        if ( g > cnt ) BIT_SET( PORTB, _BV( G_PIN ) );
-        if ( b > cnt ) BIT_SET( PORTB, _BV( B_PIN ) );
+    //    if ( cnt == 0 ) {
+    //        if ( r > cnt ) BIT_SET( PORTB, _BV( R_PIN ) );
+    //        if ( g > cnt ) BIT_SET( PORTB, _BV( G_PIN ) );
+    //        if ( b > cnt ) BIT_SET( PORTB, _BV( B_PIN ) );
+    //    }
+    //
+    //    if ( cnt >= r ) {
+    //        BIT_CLR( PORTB, _BV( R_PIN ) );
+    //    }
+    //
+    //    if ( cnt >= g ) {
+    //        BIT_CLR( PORTB, _BV( G_PIN ) );
+    //    }
+    //
+    //    if ( cnt >= b ) {
+    //        BIT_CLR( PORTB, _BV( B_PIN ) );
+    //    }
+    //
+    //    cnt++;
+
+    if (cnt < 4) {
+        if (r > cnt) {
+            BIT_SET(PORTB, _BV(R_PIN));
+        }
+
+        if (g > cnt) {
+            BIT_SET(PORTB, _BV(G_PIN));
+        }
+
+        if (b > cnt) {
+            BIT_SET(PORTB, _BV(B_PIN));
+        }
     }
-    
-    if ( cnt >= r ) {
-        BIT_CLR( PORTB, _BV( R_PIN ) );
+
+    if (cnt >= r) {
+        BIT_CLR(PORTB, _BV(R_PIN));
     }
-    
-    if ( cnt >= g ) {
-        BIT_CLR( PORTB, _BV( G_PIN ) );
+
+    if (cnt >= g) {
+        BIT_CLR(PORTB, _BV(G_PIN));
     }
-    
-    if ( cnt >= b ) {
-        BIT_CLR( PORTB, _BV( B_PIN ) );
+
+    if (cnt >= b) {
+        BIT_CLR(PORTB, _BV(B_PIN));
     }
-    
-    switch ( cnt ) {
-        case 252:   cnt = 2;    break;
-        case 253:   cnt = 3;    break;
-        case 254:   cnt = 1;    break;
-        case 251:   cnt = 0;    break;
-        default:    cnt += 4;   break;
+
+    switch (cnt) {
+        case 252:
+            cnt = 2;
+            break;
+
+        case 253:
+            cnt = 3;
+            break;
+
+        case 254:
+            cnt = 1;
+            break;
+
+        case 251:
+            cnt = 0;
+            break;
+
+        default:
+            cnt += 4;
+            break;
     }
 }
 
 
 
-ISR( TIM0_COMPA_vect ) {
+ISR(TIM0_COMPA_vect) {
     pwm();
 }
 
 
 
-static void wdt_off( void ) {
+static void wdt_off(void) {
     wdt_reset();                            // reset watchdog
     MCUSR = 0x00;                           // clear reset status
-    BIT_SET( WDTCR, _BV2( WDCE, WDE ) );    // needs to be set in order to make changes to the prescaler
+    BIT_SET(WDTCR, _BV2(WDCE, WDE));        // needs to be set in order to make changes to the prescaler
     WDTCR = 0x00;                           // turn watchdog off
 }
 
 
 
-static void wdt_on( void ) {
-    BIT_SET( WDTCR, _BV2( WDCE, WDE ) );    // needs to be set in order to make changes to the prescaler
-    WDTCR = _BV( WDE );                     // set timer to 16ms and enable reset timer
+static void wdt_on(void) {
+    BIT_SET(WDTCR, _BV2(WDCE, WDE));        // needs to be set in order to make changes to the prescaler
+    WDTCR = _BV(WDE);                       // set timer to 16ms and enable reset timer
 }
 
 
 
 static uint8_t eeprom_read(uint8_t ucAddress) {
     /* Wait for completion of previous write */
-    while(EECR & (1<<EEPE));
+    while (EECR & (1 << EEPE));
+
     /* Set up address register */
     EEAR = ucAddress;
     /* Start eeprom read by writing EERE */
-    EECR |= (1<<EERE);
+    EECR |= (1 << EERE);
     /* Return data from data register */
     return EEDR;
 }
 
 
 
-static void eeprom_write( uint8_t ucAddress, uint8_t ucData ) {
+static void eeprom_write(uint8_t ucAddress, uint8_t ucData) {
     /* Wait for completion of previous write */
-    while(EECR & (1<<EEPE));
+    while (EECR & (1 << EEPE));
+
     /* Set Programming mode */
-    EECR = (0<<EEPM1)|(0<<EEPM0);
+    EECR = (0 << EEPM1) | (0 << EEPM0);
     /* Set up address and data registers */
     EEAR = ucAddress;
     EEDR = ucData;
     /* Write logical one to EEMPE */
-    EECR |= (1<<EEMPE);
+    EECR |= (1 << EEMPE);
     /* Start eeprom write by setting EEPE */
-    EECR |= (1<<EEPE);
+    EECR |= (1 << EEPE);
 }
 
 
 
 static void eeprom_init() {
-    uint8_t i2cAddr = eeprom_read( 0x00 );
-    
+    uint8_t i2cAddr = eeprom_read(0x00);
+
     // if the I2C-Address is invalid then write sane values into the EEPROM
-    if ( i2cAddr < 0x03 or i2cAddr > 0x77 ) {
-        eeprom_write( 0x00, 0x3f );
-        eeprom_write( 0x01, 0x00 );
-        eeprom_write( 0x02, 0x00 );
-        eeprom_write( 0x03, 0x00 );
+    if (i2cAddr < 0x03 or i2cAddr > 0x77) {
+        eeprom_write(0x00, 0x3f);
+        eeprom_write(0x01, 0x00);
+        eeprom_write(0x02, 0x00);
+        eeprom_write(0x03, 0x00);
     }
 }
 
@@ -180,39 +204,74 @@ static void eeprom_init() {
 
 static void reset() {
     wdt_on();   // reset via watchdog
-    
+
     while (true) {}
 }
 
 
 
 // A callback triggered when the i2c master attempts to read from a register.
-static uint8_t i2cReadFromRegister(uint8_t reg)
-{
-    switch( reg ) {
-        case '@': return eeprom_read( 0x00 );   // this is pretty much useless but for completeness
-        case 'R': return eeprom_read( 0x01 );
-        case 'G': return eeprom_read( 0x02 );
-        case 'B': return eeprom_read( 0x03 );
-        case 'r': return r;
-        case 'g': return g;
-        case 'b': return b;
-        default: return 0;
+static uint8_t i2cReadFromRegister(uint8_t reg) {
+    switch (reg) {
+        case '@':
+            return eeprom_read(0x00);     // this is pretty much useless but for completeness
+
+        case 'R':
+            return eeprom_read(0x01);
+
+        case 'G':
+            return eeprom_read(0x02);
+
+        case 'B':
+            return eeprom_read(0x03);
+
+        case 'r':
+            return r;
+
+        case 'g':
+            return g;
+
+        case 'b':
+            return b;
+
+        default:
+            return 0;
     }
 }
 
 
 
 // A callback triggered when the i2c master attempts to write to a register.
-static void i2cWriteToRegister( uint8_t reg, uint8_t value ) {
-    switch( reg ) {
-        case '@': eeprom_write( 0x00, value ); reset(); break;
-        case 'R': eeprom_write( 0x01, value ); break;
-        case 'G': eeprom_write( 0x02, value ); break;
-        case 'B': eeprom_write( 0x03, value ); break;
-        case 'r': r = value;  break;
-        case 'g': g = value;  break;
-        case 'b': b = value;  break;
+static void i2cWriteToRegister(uint8_t reg, uint8_t value) {
+    switch (reg) {
+        case '@':
+            eeprom_write(0x00, value);
+            reset();
+            break;
+
+        case 'R':
+            eeprom_write(0x01, value);
+            break;
+
+        case 'G':
+            eeprom_write(0x02, value);
+            break;
+
+        case 'B':
+            eeprom_write(0x03, value);
+            break;
+
+        case 'r':
+            r = value;
+            break;
+
+        case 'g':
+            g = value;
+            break;
+
+        case 'b':
+            b = value;
+            break;
     }
 }
 
@@ -220,26 +279,26 @@ static void i2cWriteToRegister( uint8_t reg, uint8_t value ) {
 
 int main(void) {
     wdt_off();                          // recover from watchdog reset
-    
+
     eeprom_init();                      // sets eeprom to sane values
-    
-    DDRB = _BV3( R_PIN, G_PIN, B_PIN ); // Set up output pins
-    
-    TCCR0A |= _BV( WGM01 );             // enable CTC
+
+    DDRB = _BV3(R_PIN, G_PIN, B_PIN);   // Set up output pins
+
+    TCCR0A |= _BV(WGM01);               // enable CTC
     OCR0A = 128;                        // twice as often as with overflow
-    TCCR0B |= _BV( CS00 );              // prescale timer to the clock rate
-    TIMSK |= _BV( OCIE0A );             // enable compare match interrupt
-    
-    uint8_t i2cAddr = eeprom_read( 0x00 );
-    usiTwiSlaveInit( i2cAddr, i2cReadFromRegister, i2cWriteToRegister );
-    
-    r = eeprom_read( 0x01 );
-    g = eeprom_read( 0x02 );
-    b = eeprom_read( 0x03 );
-    
+    TCCR0B |= _BV(CS00);                // prescale timer to the clock rate
+    TIMSK |= _BV(OCIE0A);               // enable compare match interrupt
+
+    uint8_t i2cAddr = eeprom_read(0x00);
+    usiTwiSlaveInit(i2cAddr, i2cReadFromRegister, i2cWriteToRegister);
+
+    r = eeprom_read(0x01);
+    g = eeprom_read(0x02);
+    b = eeprom_read(0x03);
+
     sei();
-    
-    while( true ) {
+
+    while (true) {
         // let ISR handle the LED forever
     }
 }
